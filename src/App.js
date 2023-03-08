281198b38b3472666b63a7b861ed238dad4d7e0e
hana <hanasabih@gmail.com>
2023-03-08 08:23:25 +0200

    add header setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Routes, Route } from 'react-router-dom';
+import NavbarPg from './components/NavbarPg';
+import NoPage from './components/NoPage';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<NavbarPg />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </>
+);
 
 export default App;
---
+++ src/components/NavbarPg.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NavbarPg = () => <div />;
+
+export default NavbarPg;
