2e51d6691bf319c27ffc310590b1fd5479072717
hana <hanasabih@gmail.com>
2023-03-10 09:46:58 +0200

    final version

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -6,8 +6,30 @@ const MyProfile = () => {
   const rocketsList = useSelector((state) => state.rockets);
   const myReserveRockets = rocketsList.filter((rockets) => rockets.reserved);
 
+  const missionsList = useSelector((state) => state.missions.list);
+  const reservedMissions = missionsList.filter((mission) => mission.reserved);
   return (
     <Container className="mt-3 d-flex gap-5">
+      <Table
+        striped
+        bordered
+        hover
+        variant="dark"
+        className=" border border-warning"
+      >
+        <thead>
+          <tr className="fs-3 text-warning">
+            <th>My Missions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservedMissions.map((mission) => (
+            <tr key={mission.id}>
+              <td className="my-profile-td fs-5">{mission.mission_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
       <Table
         striped
         bordered
